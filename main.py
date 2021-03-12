11bdde265b6257493880fb3a2f4db1e4dd858bb1
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-03-12 20:09:19 +0000

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-legendaryChance = float(input("What is your legendary chance?"))
-boxesOpened = int(input("How many boxes did you open?"))
-
-chance = 1 - ((1 - legendaryChance)**boxesOpened)
-print(chance, "%")
