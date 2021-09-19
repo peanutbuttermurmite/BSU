8c8cf05ab544e9558f6dba869f8d9cba23fd6196
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-09-19 20:14:15 +0100

    Update bsuinstaller.sh

--- bsu-install/bsuinstaller.sh
+++ bsu-install/bsuinstaller.sh
@@ -3,7 +3,7 @@ echo "---> Installing Packages"
 sudo apt-get install python3 python3-tk python3-pip -y
 echo "---> Downloading BSU + Python dependencies"
 git clone https://github.com/peanutbuttermurmite/BSU.git
-pip3 install enquiries selenium PySimpleGUI
+pip3 install enquiries selenium PySimpleGUI replit
 cd BSU/bsu-install
 chmod a+x bsu
 cd ..
@@ -11,7 +11,6 @@ cd ..
 mv BSU /opt
 "---> Installing BSU + updaterbsu"
 sudo ln -s /opt/BSU/bsu-install/bsu /usr/local/bin/bsu
-cp -r bsu.desktop ~/.local/share/applications
 echo "Installed Successfully"
 echo "Run BSU by typing "bsu --run" into your terminal or use the .desktop file"
 
