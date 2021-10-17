0864c389949f5418263d5c432a39aa7914229c01
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-10-17 21:39:13 +0100

    Update bsuinstaller.sh
    Added progress bar to install

--- bsu-install/bsuinstaller.sh
+++ bsu-install/bsuinstaller.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 echo "---> Installing Packages"
+echo -ne '>>>>>>>>                       [25%]\r'
 sudo apt-get install python3 python3-tk python3-pip -y
 echo "---> Downloading BSU + Python dependencies"
 git clone https://github.com/peanutbuttermurmite/BSU.git
-pip3 install enquiries selenium PySimpleGUI 
+pip3 install enquiries selenium PySimpleGUI
+echo -ne '>>>>>>>>>>>>>>>                [50%]\r'
 cd BSU/bsu-install
 chmod a+x bsu
 cd ..
 cd ..
-mv BSU /opt
+sudo mv BSU /opt
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>        [75%]\r'
 "---> Installing BSU + updaterbsu"
 sudo ln -s /opt/BSU/bsu-install/bsu /usr/local/bin/bsu
+sudo cp -r bsu.desktop ~/.local/share/applications
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 echo "Installed Successfully"
 echo "Run BSU by typing "bsu --run" into your terminal or use the .desktop file"
-
