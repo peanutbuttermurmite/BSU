b3c4ad4f6fccf1306993b525f1b60d723e59fc87
peanutbuttermurmite <mincecraft46@gmail.com>
2021-10-31 16:06:10 +0000

    Update bsuinstaller.sh
    Fully integrate YAD with installation script.

--- bsu-install/bsuinstaller.sh
+++ bsu-install/bsuinstaller.sh
@@ -1,19 +1,21 @@
-
+#!/bin/bash
 sudo ln -s /opt/BSU/bsu-install/bsu /usr/local/bin/bsu
-sudo cp -r bsu.desktop ~/.local/share#!/bin/bash
+sudo cp -r bsu.desktop ~/.local/share
 sudo apt install python3 python3-tk python3-pip python3-pil python3-pil.imagetk yad -y > /dev/null 
 yad --progress \
   --title="Dependency Install" \
   --text="All dependencies installed" \
   --percentage=33
+  --autoclose
 git clone https://github.com/peanutbuttermurmite/BSU.git
 pip3 install enquiries selenium PySimpleGUI
 yad --progress \
   --title="Python packages " \
   --text="Setting up..." \
   --percentage=66
+  --autoclose
 cd BSU/bsu-install
-chmod a+x bsu
+chmod ugo+rwx bsu
 cd ..
 cd ..
 sudo mv BSU /opt/applications
@@ -21,6 +23,5 @@ yad --progress \
   --title="BSU has no been set up" \
   --text="Installation Complete" \
   --percentage=100
-printf -- "Installed Successfully"
-printf -- "Run BSU by typing "bsu --run" into your terminal or use the .desktop file"
-printf -- "Use bsu --help to show all commands (WARNING:use sudo before commands)"
+  --autoclose
+yad --text "Run BSU by typing "bsu --run" into your terminal or use the .desktop file.Use bsu --help to show all commands (WARNING:use sudo before commands)"
