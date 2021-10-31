c030773a62072b74030d06e5b6e53fed4f8388f5
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-10-31 13:57:14 +0000

    Update bsuinstaller.sh

--- bsu-install/bsuinstaller.sh
+++ bsu-install/bsuinstaller.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-printf "---> Installing Packages"
+printf -- "---> Installing Packages"
 echo -ne '>>>>>>>>                       [25%]\r'
 sudo apt-get install python3 python3-tk python3-pip python3-pil python3-pil.imagetk
-printf "---> Downloading BSU + Python dependencies"
+printf -- "---> Downloading BSU + Python dependencies"
 git clone https://github.com/peanutbuttermurmite/BSU.git
 pip3 install enquiries selenium PySimpleGUI
 echo -ne '>>>>>>>>>>>>>>>                [50%]\r'
@@ -12,9 +12,9 @@ cd ..
 cd ..
 sudo mv BSU /opt
 echo -ne '>>>>>>>>>>>>>>>>>>>>>>>        [75%]\r'
-printf "---> Installing BSU + updaterbsu"
+printf -- "---> Installing BSU + updaterbsu"
 sudo ln -s /opt/BSU/bsu-install/bsu /usr/local/bin/bsu
 sudo cp -r bsu.desktop ~/.local/share/applications
 echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
-printf "Installed Successfully"
-printf "Run BSU by typing "bsu --run" into your terminal or use the .desktop file"
+printf -- "Installed Successfully"
+printf -- "Run BSU by typing "bsu --run" into your terminal or use the .desktop file"
