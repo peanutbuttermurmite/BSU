5fa07b96a207b796245f1ef6a3f51f2f100caf98
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-12-05 15:39:52 +0000

    Update bsuinstaller.sh

--- bsu-install/bsuinstaller.sh
+++ bsu-install/bsuinstaller.sh
@@ -3,36 +3,32 @@ if [ "$(id -u)" -ne 0 ]; then
 	echo "run this script as root" >&2
 	exit 1
 fi
-if [ -f /etc/os-release ]; then
-    # freedesktop.org and systemd
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
-    # linuxbase.org
-    OS=$(lsb_release -si)
-    VER=$(lsb_release -sr)
-elif [ -f /etc/lsb-release ]; then
-    # For some versions of Debian/Ubuntu without lsb_release command
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
-elif [ -f /etc/debian_version ]; then
-    # Older Debian/Ubuntu/etc.
-    OS=Debian
-    VER=$(cat /etc/debian_version)
-elif [ -f /etc/SuSe-release ]; then
-    # Older SuSE/etc.
-    ...
-elif [ -f /etc/redhat-release ]; then
-    # Older Red Hat, CentOS, etc.
-    ...
-else
-    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-    OS=$(uname -s)
-    VER=$(uname -r)
+if ! command -v which &> /dev/null
+then
+    echo "Which could not be found"
+    exit 1
 fi
-ubu="Ubuntu"
+if ! command -v git &> /dev/null
+then
+    echo "Git could not be found"
+    exit 1
+fi
+if [[ `which yum` ]]; then
+   OS="CentOS"
+elif [[ `which apt` ]]; then
+   OS="Debian"
+elif [[ `which apk` ]]; then
+   OS="Alpine"
+elif [[ `which zypper` ]]; then
+   OS="OpenSuse"
+elif [[ `which pacman` ]]; then
+   OS="Arch"
+elif [[ `which dnf` ]]; then
+   OS="Fedora"
+
+else
+   IS_UNKNOWN=1
+fi
 suse="OpenSuse"
 alpine2="Alpine"
 cent="CentOS"
@@ -40,9 +36,6 @@ redhat="Fedora"
 deb="Debian"
 pacman="Arch"
 
-if (( OS==ubu )) 
-then
-    package_manager="apt install -y"
 fi 
 if (( OS==suse ))
 then 
