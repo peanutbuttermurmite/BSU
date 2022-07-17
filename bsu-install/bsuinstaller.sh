f8efd40481687a0c1b6d8b4996387ee924e866a9
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2022-07-17 14:33:57 +0100

    Update bsuinstaller.sh

--- bsu-install/bsuinstaller.sh
+++ bsu-install/bsuinstaller.sh
@@ -16,7 +16,9 @@ fi
 if [ "$rootless" = true ]; then
    git clone https://github.com/peanutbuttermurmite/easy-proot.git
    STARTUPCMD="apt update && apt install git whiptail -y"
-    "$STARTUPCMD">>easy-proot/assets/etc/skel/.bashrc
+   STARTUPCMD2="apt install git whiptail -y"
+   $STARTUPCMD>>easy-proot/assets/etc/skel/.bashrc
+   $STARTUPCMD2>>easy-proot/assets/etc/skel/.bashrc
    cd easy-proot || exit
    chmod +x start.sh
    ./start.sh
